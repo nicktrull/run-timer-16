84fb3f311f535baee23ad96a33c63576915ae082
Nick Trull <nick.trull@gmail.com>
2016-04-08 21:37:23 -0500

    run link display run info

--- app/run-detail/route.js
+++ app/run-detail/route.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(url) {
+    const myUrl = `http://tiny-tn.herokuapp.com/collections/runs-nt/${url._id}`;
+
+    return fetch(myUrl)
+    .then((results) => results.json());
+  },
 });
